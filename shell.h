28c5f1db6059c8e34de4e9352ca8bc89eb23e413
Basem Gamal <bassemgamalosman12@gmail.com>
2023-11-15 23:12:02 -0800

    Fixing errors

--- shell.h
+++ shell.h
@@ -156,13 +156,10 @@ int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
 /* lists.c */
-unsigned int _strlen(char *);
-list_t *add_node(list_t **, const char *);
-list_t *add_node_end(list_t **, const char *);
-int delete_nodeint_at_index(listint_t **, unsigned int);
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
-
-/* lists3.c */
+int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
 /* lists2.c */
